10c42d3bd6b3784b476e69734bab0e698fdbe34b
Alex <alexbachurin91@gmail.com>
2022-04-14 18:26:01 +0300

    signIn with google redirect functionality

--- src/routes/SignIn/SignIn.js
+++ src/routes/SignIn/SignIn.js
@@ -1,13 +1,27 @@
-import React from "react";
+import { getRedirectResult } from "firebase/auth";
+import React, { useEffect } from "react";
 import {
+	auth,
 	signInWithGooglePopup,
 	createUserDocumentFromAuth,
+	signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase";
 const SignIn = () => {
 	const logGoogleUser = async () => {
 		const res = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentFromAuth(res.user);
 	};
+
+	//sign in with google redirect functionality
+	useEffect(() => {
+		const loginWithGoogleRedirect = async () => {
+			const res = await getRedirectResult(auth);
+			if (res) {
+				const userDocRef = await createUserDocumentFromAuth(res.user);
+			}
+		};
+		loginWithGoogleRedirect();
+	}, []);
 	return (
 		<div>
 			SignIn
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -28,6 +28,8 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+	signInWithRedirect(auth, provider);
 
 //our db
 export const db = getFirestore();
