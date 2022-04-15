ee587bdfdf158d09cb82445b2d78fd73e5ba27e1
Alex <alexbachurin91@gmail.com>
2022-04-15 16:12:27 +0300

    product card styling

--- src/routes/Shop/Shop.js
+++ src/routes/Shop/Shop.js
@@ -1,16 +1,14 @@
 import React from "react";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import { useProductsContext } from "../../contexts/ProductsContext";
+import "./shop.styles.scss";
 
 const Shop = () => {
 	const { products } = useProductsContext();
 	return (
-		<div>
+		<div className="products-container">
 			{products.map((item) => {
-				return (
-					<div key={item.id}>
-						<h1>{item.name}</h1>
-					</div>
-				);
+				return <ProductCard key={item.id} {...item} />;
 			})}
 		</div>
 	);
